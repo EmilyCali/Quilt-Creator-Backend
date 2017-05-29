075b5cc0a1514ec14d177c27e7b759c58183dada
emilycali <cali_emily@bah.com>
2017-05-29 17:29:52 -0400

    quilt_blocks owned by user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
   #built in password securing
   has_secure_password
   #one to many relationship
-  has_many :quilts
+  #has_many :quilts
 
+  has_many :quilt_blocks
   #validates_associated :quilts
 end
